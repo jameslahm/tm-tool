340d51a7adfd98e0c6efba28050fe56450692e54
wangao <1366463855@qq.com>
2020-07-02 19:09:52 +0800

    fix: add ignore

--- packages/tm-ui/build/web_modules/react.js
+++
@@ -1,6 +0,0 @@
-import { r as react } from './common/index-4727b338.js';
-export { r as default } from './common/index-4727b338.js';
-
-var useEffect = react.useEffect;
-var useState = react.useState;
-export { useEffect, useState };
